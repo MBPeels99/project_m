7cd7ba80fe9343ce2baa7aef4f8b9fec73cd4ea2
MBPeels99 <peelsmike3@gmail.com>
2024-01-15 21:44:37 +0100

    first commit

---
+++ src/test/java/com/peelsincorparate/project_m/ProjectMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peelsincorparate.project_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
